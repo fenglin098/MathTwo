7f02a3c81aab4468e27f0aa85d685396e4b6aae3
fenglin098 <feng.lin098@gmail.com>
2019-10-21 16:42:18 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main( String[] args) {
+        float mynumber;
+        mynumber = 2;
+        double myothernumber;
+        myothernumber = 1.7938;
+        System.out.println(mynumber+myothernumber);
+
+    }
+}
